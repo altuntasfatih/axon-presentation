f258fdfc3ffdd0134cbab4bd81deeb94e100b8dc
Fatih Altuntaş <altuntasfatih42@gmail.com>
2021-05-15 17:48:32 +0300

    first commit

---
+++ wallet-demo/src/main/java/com/demo/wallet/WalletDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletDemoApplication.class, args);
+    }
+
+}
---
+++ wallet-demo/src/test/java/com/demo/wallet/WalletDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
