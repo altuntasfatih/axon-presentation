eeded8c310d74d4bc5f9ffcb976f7a358100cf1e
Fatih Altuntaş <altuntasfatih42@gmail.com>
2021-06-05 12:19:35 +0300

    Rename eventsourcingHandler method name

--- wallet-demo/src/main/java/com/demo/wallet/domain/Wallet.java
+++ wallet-demo/src/main/java/com/demo/wallet/domain/Wallet.java
@@ -50,7 +50,7 @@ public class Wallet {
     }
 
     @EventSourcingHandler//it used to form aggregate current state.(a.k.s Aggregation)
-    protected void handle(DepositedEvent event) {
+    protected void on(DepositedEvent event) {
         //the place where you would change the aggregate state.
         this.balance = this.balance.add(event.getDepositAmount());
     }
@@ -66,7 +66,7 @@ public class Wallet {
     }
 
     @EventSourcingHandler
-    protected void handle(PaidEvent event) {
+    protected void on(PaidEvent event) {
         //the place where you would change the aggregate state.
         this.balance = this.balance.subtract(event.getPayAmount());
     }
@@ -82,7 +82,7 @@ public class Wallet {
     }
 
     @EventSourcingHandler
-    protected void handle(WithdrawnEvent event) {
+    protected void on(WithdrawnEvent event) {
         //the place where you would change the aggregate state.
         this.balance = this.balance.subtract(event.getWithdrawAmount());
     }
